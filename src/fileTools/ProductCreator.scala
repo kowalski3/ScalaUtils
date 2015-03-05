52ba8fef40de0d03c133386289128e85d971e2bf
JoolsF <joolsmf@gmail.com>
2015-03-05 16:59:50 +0000

    Creating class for albums with multiple discs

--- src/fileTools/ProductCreator.scala
+++ src/fileTools/ProductCreator.scala
@@ -25,35 +25,65 @@ class ProductCreator {
     for (lines <- file.getLines()) {
        // Get values from line
        val line = lines.split(",") 
-       val albumid:String = line(0) ;  val albumTrackNo:String = line(1) ; val sfid:String = line(2) ; val artistTrackName =line(3)
+
+       val albumid:String = line(0)
+       val discNo:String = line(1) //need better of handling this
+       val albumTrackNo:String = line(2)
+       val sfid:String = line(3) 
+       val artistTrackName =line(4)
        
        // Check if album already in map and add if not
        val result = albumMap.get(albumid)
        result match{
-         case None => albumMap.put(albumid, new Album(albumid))
+         case None => if(discNo.length == 0) 
+                         albumMap.put(albumid, new AlbumNoDisc(albumid))
+                       else
+                         albumMap.put(albumid, new AlbumWithDisc(albumid))
          case Some(x) =>
        }
-       //add album to map
-       albumMap.get(albumid).get.trackList.+=(new Track(albumTrackNo, sfid, artistTrackName))
+
+       albumMap.get(albumid).get.addTrack(albumTrackNo, sfid, discNo, artistTrackName)
+         
     }
-     
-    
-   }//constructor ends here
+
+    //constructor ends here
   
  }
+}
 
 
+trait Album{
+  def addTrack(albumTrackNo:String, sfid:String, discNo: String, artistTrackName:String)
+  
+}
 
 
-
-
-
-
-
-class Album(val albumid: String){  
-  val trackList = ListBuffer[Track]()
+class AlbumNoDisc(val albumid: String) extends Album{  
+  private val trackList = ListBuffer[Track]()
+  
+ @Override 
+ def addTrack(albumTrackNo:String, sfid:String, discNo: String, artistTrackName:String) = {
+    trackList += new Track(albumTrackNo, sfid, artistTrackName)
   }
-            
+}
+
+class AlbumWithDisc(albumid: String) extends Album{
+  private val discMap = Map[String,ListBuffer[Track]]() //map from discno to tracklist
+  
+  @Override 
+  def addTrack(albumTrackNo:String, sfid:String, discNo: String, artistTrackName:String) = {
+    val result = discMap.get(discNo)
+    result match{
+      case None =>   discMap.put(discNo, new ListBuffer[Track]())
+      case Some(x) =>
+     }
+    
+    
+    discMap.get(discNo).get.+=(new Track(albumTrackNo, sfid, artistTrackName))
+    
+  }
+}
+           
   
 class Track(val albumTrackNo:String, 
             val sfid: String, 
